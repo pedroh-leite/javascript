3b898a8117bd6acb0595a30234164e48dbe87f67
pedroh-leite <pedrohleite.alves@gmail.com>
2023-02-08 15:22:14 -0300

    aprendendo variáveis

---
+++ estudos/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,52 @@
+     /* let js = "amazing";
+      console.log(40+8+23-10);
+
+      console.log("pedro");
+      console.log(23);
+
+      let firstName = "peter";
+      console.log(firstName);
+      console.log(firstName);
+      console.log(firstName);
+
+    //   Nomes das variáveis
+      let peo_sa = "PS";
+      let $function = 27;
+
+      let person = "peo";
+      let PI = 3.1415;
+
+      let javascriptIsFun = true;
+      console.log(javascriptIsFun);
+
+        //console.log(typeof true);
+      console.log(typeof javascriptIsFun);
+        //   console.log(typeof 23);
+        //   console.log(typeof "pedro");
+
+      javascriptIsFun = 'YES';
+      console.log(typeof javascriptIsFun);
+
+      let year;
+      console.log(year);
+      console.log(typeof year);
+
+      year = 1991;
+      console.log(year);
+      console.log(typeof year);*/
+
+      let age = 30;
+      age = 31;
+
+      const birthYear = 2003;
+
+      var job = "hr";
+      job = "programmer";
+
+      lastName = "Leite";
+      console.log(lastName)
+
+
+
+
+
