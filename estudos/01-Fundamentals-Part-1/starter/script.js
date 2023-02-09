c1e9d12c826cac681cee331495ab28a68df8aa34
pedroh-leite <pedrohleite.alves@gmail.com>
2023-02-09 15:49:12 -0300

    aprendendo operadores em js

--- estudos/01-Fundamentals-Part-1/starter/script.js
+++ estudos/01-Fundamentals-Part-1/starter/script.js
@@ -33,7 +33,7 @@
 
       year = 1991;
       console.log(year);
-      console.log(typeof year);*/
+      console.log(typeof year);
 
       let age = 30;
       age = 31;
@@ -45,6 +45,85 @@
 
       lastName = "Leite";
       console.log(lastName)
+
+      // Operadores de matemática
+      const now = 2037;
+      const agePedro = now - 2003;
+      const ageSa = now - 2018;
+      console.log(agePedro, ageSa);
+
+      console.log(agePedro * 2, agePedro / 10, 2**3);
+
+      const firstName = "Pedro";
+      const lastName = "Leite";
+      const space = " ";     
+      console.log(firstName + " " + lastName)
+
+      //Operadores de atribuição 
+      let x = 10 + 5; //x = 15
+      x += 10 // x = x + 10 = 25
+      x *= 4 // x = x * 4 = 100
+      x++ // x = x + 1
+      x-- // x = x-1
+      x--
+      console.log(x);
+
+      // Operadores de comparação
+      console.log(agePedro > ageSa);
+      console.log(agePedro >= 18);
+
+      const fullAge = agePedro >= 18;
+      console.log(now - 2003 > now - 2018)
+
+      const now = 2037;
+      const agePedro = now - 2003;
+      const ageSa = now - 2018;
+     
+      console.log(now - 2003 > now - 2018)
+
+      let x, y;
+      x = y = 25-10-5;
+      console.log(x, y);
+      
+      const idadeMedia = (agePedro + ageSa) / 2      
+      console.log(agePedro, ageSa, idadeMedia)
+
+      const markWeight = 78
+      const johnWeight = 92
+      const markHeight = 1.69
+      const johnHeight = 1.95
+      
+
+      const markBmi = markWeight / (markHeight * markHeight)
+      console.log(markBmi)
+
+      const johnBmi = johnWeight / (johnHeight * johnHeight)
+      console.log(johnBmi)
+
+      let markHigherBmi = (markBmi>johnBmi)
+      console.log(markHigherBmi)*/
+
+      const firstName = "Pedro";
+      const job = "programmer";
+      const birthYear = 2003;
+      const year = 2037; 
+
+      const pedro = "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+      console.log(pedro)
+
+      const pedroNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+      console.log(pedroNew)
+
+      console.log(`Just a regular string`)
+
+      console.log('String with \n\
+        multiple \n\
+        lines')
+
+        console.log(`String with 
+        multiple 
+        lines`)
+
 
 
 
