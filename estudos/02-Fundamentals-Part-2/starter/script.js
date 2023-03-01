ade5ce12e4df71ad3b12134367840d24652f0c00
pedroh-leite <pedrohleite.alves@gmail.com>
2023-03-01 13:25:25 -0300

    finalizando modulo 2

--- estudos/02-Fundamentals-Part-2/starter/script.js
+++ estudos/02-Fundamentals-Part-2/starter/script.js
@@ -416,7 +416,7 @@ while(rep<=10) {
     console.log(`WHILE: lifting weights repetition ${rep}`); 
 
     rep++;
-}*/
+}
 
 let dice = Math.trunc(Math.random() * 6) + 1
 
@@ -426,7 +426,33 @@ while(dice !== 6){
     if(dice === 6) console.log("Loop is about to end...");
 }
 
+const calcTip = function(bill){
+return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
 
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for(let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i])
+}
+
+console.log(bills, tips, totals);
+
+const calcAverage = function(arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        //sum = sum + arr[i];
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));*/
 
 
 
