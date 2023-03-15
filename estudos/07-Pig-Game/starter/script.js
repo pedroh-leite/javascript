85c5730670bcd01be0e40ea5ed5c1405e226afd9
pedroh-leite <pedrohleite.alves@gmail.com>
2023-03-15 15:18:46 -0300

    Finalizando pig-game

--- estudos/07-Pig-Game/starter/script.js
+++ estudos/07-Pig-Game/starter/script.js
@@ -15,36 +15,79 @@ const btnHold = document.querySelector(".btn--hold")
 
 
 //starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0]
-let currentScore = 0;
-let activePlayer = 0;
+const init = function(){
+   
+    scores = [0, 0]
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    
+    diceEl.classList.add("hidden");
+    player0El.classList.remove("player--winner");
+    player1El.classList.remove("player--winner");
+    player0El.classList.add("player--active");
+    player1El.classList.remove("player--active");
+}
+
+init();
+
+const switchPlayer = function(){
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+activePlayer = activePlayer === 0 ? 1 : 0; 
+currentScore = 0;
+player0El.classList.toggle("player--active");
+player1El.classList.toggle("player--active");
+}
 
 //Rolling dices
 btnRoll.addEventListener("click", function() {
-    //1. Gerar uma jogada de dados aleatória
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice)
+    if(playing){
+        //1. Gerar uma jogada de dados aleatória
+        const dice = Math.trunc(Math.random() * 6) + 1;
 
-    //2. Exibir os dados
-     diceEl.classList.remove("hidden");
-     diceEl.src = `dice-${dice}.png`; 
-    //3. Checar se há uma rolagem: Se sim, 
-    if(dice !== 1) {
+        //2. Exibir os dados
+        diceEl.classList.remove("hidden");
+        diceEl.src = `dice-${dice}.png`; 
+        //3. Checar se há uma rolagem: Se sim, 
+        if(dice !== 1) {
         //Adicionar aos dados a pontuação atual
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    } else {
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } else {
         //trocar para o proximo jogador
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0; 
-        currentScore = 0;
-        player0El.classList.toggle("player--active");
-        player1El.classList.toggle("player--active");
-
+        switchPlayer();
+        }
     }
+});
+
+btnHold.addEventListener("click", function(){
+    if(playing){
+
+    //Adicionar pontuação atual do jogador ativo
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    //Checar se o jogador fez 100 pontos
+    
+        if(scores[activePlayer] >= 10){
+        //Se sim, finalizar o jogo
+            playing = false;
+            diceEl.classList.add("hidden");
+            document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
+            document.querySelector(`.player--${activePlayer}`).classList.remove     ("player--active");    
+        } else {
+        //Se não, trocar jogador
+            switchPlayer();
+        }
+    } 
 })
 
+//reiniciar o jogo
+btnNew.addEventListener("click", function(){init();})
