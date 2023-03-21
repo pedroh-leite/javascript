e1c34f9c37a81cd634c8d11372cda661a66bab8f
pedroh-leite <pedrohleite.alves@gmail.com>
2023-03-21 17:07:21 -0300

    Aprendendo sobre Escopo

--- estudos/08-Behind-the-Scenes/starter/script.js
+++ estudos/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+
+    function printAge(){
+        const output = ` ${firstName}, you are ${age} born in ${birthYear}`
+        console.log(output);
+      
+        if(birthYear >= 1981 && birthYear <= 2005){
+            var millenial = true;
+            const firstName = "Jonas"; 
+            const str = `Oh, and you're a millenial, ${firstName}`;
+            console.log(str)
+
+            function add(a, b){
+                return a + b
+            }
+            }
+        
+            console.log(millenial)           
+            add(2, 3)
+        }
+        
+
+        printAge();
+    
+        return age;
+
+    }
+
+    
+
+
+const firstName = "Pedro";
+calcAge(2003);
+
+//console.log(age);
