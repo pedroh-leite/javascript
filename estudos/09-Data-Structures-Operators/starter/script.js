c1d6fcade00f42bf230ed3a526a3703f5fd84c3a
pedroh-leite <pedrohleite.alves@gmail.com>
2023-04-12 15:51:48 -0300

    Working with strings

--- estudos/09-Data-Structures-Operators/starter/script.js
+++ estudos/09-Data-Structures-Operators/starter/script.js
@@ -54,12 +54,245 @@ const restaurant = {
 
 };
 
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+
+document.querySelector("button").addEventListener("click", function() {
+  const text = document.querySelector("textarea").value;
+  const rows =  text.split("\n");
+  console.log(rows);
+
+  for(const [i, row] of rows.entries() ) {
+    const [first, second] = row.toLowerCase().trim().split("_");  
+    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+  }
+});
+
+// underscore_case => underscoreCase
+//  first_name
+// Some_Variable 
+//   calculate_AGE
+// delayed_departure
+
+
+
+
+
+
+
+
+
 
 
 
 
 
 /*
+////////////////////////////////////////////////
+//WORKING With Strings Pt. 3
+console.log("a+very+nice+string".split("+"));
+console.log("Pedro Leite".split(" "));
+
+const [firstName, lastName] = "Pedro Leite".split(" ")
+
+const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+console.log(newName);
+
+const capitalizeName = function(name) {
+  const names = name.split(" ");
+  const namesUpper = []
+
+  for(const n of names){
+    //namesUpper.push(n[0].toUpperCase() + n.slice(1))
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()))
+  }
+  console.log(namesUpper.join(" "));
+};
+
+capitalizeName("jessica ann smith davies");
+capitalizeName("pedro leite");
+
+
+//Padding
+const message = "Go to gate 23!";
+console.log(message.padStart(20, "+").padEnd(30, "+"));
+console.log("Pedro".padStart(25, "+").padEnd(30, "+"));
+
+const maskCreditCard = function(number) {
+  const str = number + "";
+  const last = str.slice(-4);
+  return last.padStart(str.length, "*");
+
+}
+
+console.log(maskCreditCard(45654513));
+console.log(maskCreditCard(465453321645653));
+console.log(maskCreditCard("6541258522563132131"));
+
+
+//Repeat 
+const message2 = "Bad weather... All departues delayed...";
+console.log(message2.repeat(5))
+
+const planesInLine = function(n) {
+  console.log(`There are ${n} planes in line ${"🛬".repeat(n)}`);
+}
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+
+////////////////////////////////////////////////
+//WORKING With Strings Pt. 2
+const airline = "TAP Air Portugal";
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+//FIX Capitalization in name
+const passenger = "PeDRo"; // Pedro
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect  = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect)  
+
+//Comparing email
+const email = "hello@pedro.io";
+const loginEmail = " Hello@Pedro.Io \n";
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log( email === normalizedEmail);
+
+//Replacing
+const priceBR = "288,97R$";
+const priceUS = priceBR.replace("R$", "U$").replace(",", ".");
+console.log(priceUS);
+
+const announcement = "All passenger come to boarding door 23. Boarding door 23!";
+console.log(announcement.replace("door", "gate"));
+// console.log(announcement.replaceAll("door", "gate"));
+
+console.log(announcement.replace(/door/g, "gate"));
+
+//Booleans
+const plane = "Airbus A320neo";
+console.log(plane.includes("A320"));
+console.log(plane.includes("Boeing"));
+console.log(plane.startsWith("Air"));
+
+if(plane.startsWith("Airbus") && plane.endsWith("neo")) {
+  console.log("Part of the new Airbus family");
+};
+
+//Practice exercise
+const checkBaggage = function(items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes("knife") || baggage.includes("gun")){
+    console.log("You are NOT allowed on board");
+  } else {
+    console.log("Welcome aboard!");
+  }
+};
+
+checkBaggage("I have a laptop, some Food and a pocket Knife");
+checkBaggage("Socks and camera");
+checkBaggage("Got some snacks and a gun for protection");
+
+
+
+
+
+////////////////////////////////////////////////
+//WORKING With Strings Pt. 1
+const airline = "TAP Air Portugal";
+const plane = "A320";
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log("B737"[0]);
+
+console.log(airline.length);
+console.log("B737".length);
+
+console.log(airline.indexOf("r"));
+console.log(airline.lastIndexOf("r"));
+console.log(airline.indexOf("portugal"));
+
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(" ")));
+console.log(airline.slice(airline.lastIndexOf(" ") + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function(seat) {
+  //B and E are middle seats
+  const s = seat.slice(-1);
+  if(s === "B" || s === "E")
+  console.log("You got a middle seat 🤣")
+  else console.log("You got lucky 😎")
+}
+
+checkMiddleSeat("11B");
+checkMiddleSeat("32C");
+checkMiddleSeat("3E")
+
+console.log(new String("Pedro"));
+console.log(typeof new String("Pedro"));
+
+console.log(typeof new String("Pedro").slice(1));
+
+
+
+////////////////////////////////////////////////////////
+//CODING CHALENGE
+const gameEvents = new Map ([
+  [17, "⚽ GOAL"],
+  [36, "🔀 SUBSTITUITION"],
+  [47, "⚽ GOAL"],
+  [61, "🔀 SUBSTITUITION"],
+  [64, "🟨 YELLOW CARD"],
+  [69, "🟥 RED CARD"],
+  [70, "🔀 SUBSTITUITION"],
+  [72, "🔀 SUBSTITUITION"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🟨 YELLOW CARD"],
+]);
+
+//1.
+const events = new Set(gameEvents.values());
+console.log(events); 
+
+//2. 
+gameEvents.delete(64);
+
+//3.
+const time = [...gameEvents.keys()].pop();
+console.log(time)
+console.log(`An event happened, on average, every ${time / gameEvents.size} minutes`);
+
+
+//4.
+for(const [min, event] of gameEvents) {
+  const half = min <= 45 ? "FIRST" : "SECOND";
+  console.log(`[${half} HALF] ${min}: ${event}`)
+}
+
+
+
+
 const question = new Map([
   ["Question", "What is the best programming language in the world?"],
   [1, "C"],
