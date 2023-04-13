901a51f7edda647dcecc0d6dab18a5ba26d1fda4
pedroh-leite <pedrohleite.alves@gmail.com>
2023-04-13 13:49:09 -0300

    Finalizando o trabalho com strings e iniciando funções

--- estudos/09-Data-Structures-Operators/starter/script.js
+++ estudos/09-Data-Structures-Operators/starter/script.js
@@ -2,8 +2,17 @@
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
+  const getCode = str => str.slice(0, 3).toUpperCase();
+
+  for(const flight of flights.split("+")) {
+    const [type, from, to, time] = flight.split(";");
+    const output = `${type.startsWith("_Delayed") ? "🔴" : " "}${type.replaceAll("_", " ")} from ${getCode(from)} to ${getCode(to)} (${time.replace(":", "h")})`.padStart(45);
+    console.log(output)
+  }
+
+  /*
   const weekDays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
   const openingHours = {
     [weekDays[3]]: {
@@ -55,7 +64,7 @@ const restaurant = {
 };
 
 
-/*
+
 //////////////////////////////////////
 //Coding chalenge #4
 document.body.append(document.createElement("textarea"));
--- estudos/10-Functions/starter/script.js
+++ estudos/10-Functions/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function(flightNumber,
+     numberOfPassengers = 1,
+      price = 199 * numberOfPassengers
+      ) {
+    //ES5
+    // numberOfPassengers = numberOfPassengers || 1;
+    // price = price || 199;
+
+    const boooking = {
+        flightNumber,
+        numberOfPassengers,
+        price
+    };
+    console.log(boooking);
+    bookings.push(boooking);
+};
+
+createBooking("LH123");
+createBooking("LH123",2, 800);
+createBooking("LH123",2);
+createBooking("LH123",5);
+
+createBooking("LH123",undefined ,1000)
