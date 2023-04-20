459b64a7208487287237719516d47746ca9c0736
pedroh-leite <pedrohleite.alves@gmail.com>
2023-04-20 16:12:11 -0300

    Atualizando estudos com arrays

--- estudos/11-Arrays-Bankist/starter/script.js
+++ estudos/11-Arrays-Bankist/starter/script.js
@@ -188,12 +188,21 @@ currenciesUnique.forEach(function(value, key, map){
 })
 */
 
-const checkDogs = function(dogsJulia, dogsKate) {
+const checkDogs = function (dogsJulia, dogsKate) {
   const dogsJuliaCorrected = dogsJulia.slice();
   dogsJuliaCorrected.splice(0, 1);
   dogsJuliaCorrected.splice(-2);
-  //dogsJulia.slice(1, 3);
 
-  const
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  console.log(dogs);
+
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy🐶`);
+    }
+  });
 };
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
