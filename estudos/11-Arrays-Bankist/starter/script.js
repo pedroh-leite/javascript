f537467bbec126f0d5e0528da1b908fbc2c14f25
pedroh-leite <pedrohleite.alves@gmail.com>
2023-04-28 15:30:51 -0300

    Working with numbers

--- estudos/11-Arrays-Bankist/starter/script.js
+++ estudos/11-Arrays-Bankist/starter/script.js
@@ -10,6 +10,12 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDate: [
+
+  ]
+
+
 };
 
 const account2 = {
@@ -75,7 +81,7 @@ const displayMovements = function (movements, sort = false) {
           <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-          <div class="movements__value">${mov}€</div>
+          <div class="movements__value">${mov.toFixed(2)}€</div>
         </div>
     `;
 
@@ -85,7 +91,7 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, cur) => acc + cur, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
@@ -97,7 +103,7 @@ const calcDisplaySummary = function (acc) {
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -142,7 +148,7 @@ btnLogin.addEventListener('click', function (e) {
 
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +(inputLoginPin.value)) {
     //Display UI and a Message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -159,7 +165,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +(inputTransferAmount.value);
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -183,7 +189,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     //Add movement
@@ -200,7 +206,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +(inputClosePin.value) === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -647,7 +653,7 @@ const convertTitleCase = function(title) {
 console.log(convertTitleCase("this is a nice title"));
 console.log(convertTitleCase("this is a LONG title but not too long"));
 console.log(convertTitleCase("and here is another title  with an EXAMPLE"));
-*/
+
 
 const dogs = [
   {weight: 22, curFood: 250, owners: ["Alice", "Bob"]}, 
@@ -697,3 +703,120 @@ console.log(dogs.filter(checkEating));
 //8.
 const dogsSorted = dogs.slice().sort((a,b) => a.recFood-b.recFood)
 console.log(dogsSorted);
+
+console.log(23 === 23.0);
+
+//Base 10 - 0 to 9
+//Binary base - 0 1
+console.log(0.1 + 0.2);
+console.log( 0.1 + 0.2 === 0.3 );
+
+console.log(Number(23));
+console.log(+"23");
+
+//Parsing  
+console.log(Number.parseInt("30px", 10));
+console.log(Number.parseInt("e23", 10));
+
+console.log(Number.parseInt("2.5rem"));
+console.log(Number.parseFloat("2.5rem"));
+
+//console.log(parseFloat("2.5rem"));
+
+//Check if value is a NaN
+console.log(Number.isNaN(20));
+console.log(Number.isNaN(+"20X"));
+console.log(Number.isNaN(23 / 0));
+
+//Checking if value is a number
+console.log(Number.isFinite(20));
+console.log(Number.isFinite("20")); 
+console.log(Number.isFinite(+"20X")); 
+console.log(Number.isFinite(23 / 0)); 
+
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23 / 0));
+
+
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1/2));
+console.log(8 ** (1/3));
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, "23", 11, 2));
+console.log(Math.max(5, 18, "23px", 11, 2));
+
+console.log(Math.min(5, 18, 23, 11, 2));
+
+console.log(Math.PI * Number.parseFloat("10px") ** 2);
+
+console.log(Math.trunc(Math.random() * 6 ) + 1);
+
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min;
+//0...1 -> 0...(max-min) -> min...max
+//console.log(randomInt(10, 20));
+
+//Rounding integers
+console.log(Math.trunc(23.3));
+
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor (23.9));
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.trunc(-23.3));
+console.log(Math.floor(-23.3));
+
+//Rounding decimals
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2))
+
+
+console.log(5 % 2);
+console.log(5/2);
+
+console.log(8%3);
+console.log(8/3);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(534));
+
+labelBalance.addEventListener("click", function(){
+[...document.querySelectorAll(".movement__row")].forEach(function(row, i) {
+  if(i % 2 === 0) row.style.backgroundColor = "orange";
+  if(i % 3 === 0) row.style.backgroundColor = "blue";
+});
+});
+*/
+
+//287,460,000,000 
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.14_15;
+console.log(PI);
+
+console.log(Number("230_000"));
+console.log(parseInt("230_000"));
+
+ 
