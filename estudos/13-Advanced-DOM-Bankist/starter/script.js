8a0b1c2c68feb11858827591adf6cc3da6f7dfc2
pedroh-leite <pedrohleite.alves@gmail.com>
2023-05-04 17:31:46 -0300

    Att site

--- estudos/13-Advanced-DOM-Bankist/starter/script.js
+++ estudos/13-Advanced-DOM-Bankist/starter/script.js
@@ -1,12 +1,18 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -31,8 +37,115 @@ document.addEventListener('keydown', function (e) {
 });
 
 //////////////////////////////////////////////////////////////
+//Button scrolling
+
+btnScrollTo.addEventListener("click", function(e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+  console.log("height and width viewport", document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+  //  Scrolling
+  //  window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //    top: s1coords.top + window.pageYOffset,
+  //    behavior: "smooth",
+  // });
+
+  section1.scrollIntoView({behavior: "smooth"})
+});
+
 //////////////////////////////////////////////////////////////
+//Page navigation
+
+//1. Add event listener to common parent element
+//2. Determine what element originated the event
+
+document.querySelector(".nav__links").addEventListener("click", function(e) {
+  e.preventDefault()
+
+  //Matching strategy
+  if(e.target.classList.contains("nav__link")) {
+    const id =e.target.getAttribute("href"); 
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+   });
+  };
+});
+
+//Tabbed component
+
+
+tabsContainer.addEventListener("click", function(e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  //Guard clause
+  if(!clicked) return;
+
+  //Remove active classes
+  tabs.forEach(t => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach(c => c.classList.remove("operations__content--active"));
+
+  //Activate tab
+  clicked.classList.add("operations__tab--active");
+
+  //Activate content area
+  document
+  .querySelector(`.operations__content--${clicked.dataset.tab}`)
+  .classList.add("operations__content--active");
+});
+
+//Menu fade animation
+const handleHover = function(e) {
+  if(e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const sibling = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    sibling.forEach(el => {
+      if(el !== link) el.style.opacity = this;
+    })
+    logo.style.opacity = this;
+  }
+}
+
+//Passing "arguments" into handler
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+
+
+
 /*
+
+// tabs.forEach(t=>t.addEventListener("click", () => console.log("TAB")));
+
+
+//Page navigation
+// document.querySelectorAll(".nav__link").forEach(function(el) {
+//   el.addEventListener("click", function(e){
+//     e.preventDefault();
+//     const id =this.getAttribute("href"); 
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({
+//       behavior: "smooth",
+//     })
+//   });
+// });
+
+
+
+
 //Selecting elements
 console.log(document.documentElement);
 console.log(document.head);
@@ -114,34 +227,7 @@ logo.classList.contains("c"); // not includes
 
 //Don't use
 logo.className = "jonas"
-*/
 
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
-
-btnScrollTo.addEventListener("click", function(e) {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
-
-  console.log(e.target.getBoundingClientRect());
-
-  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
-
-  console.log("height and width viewport", document.documentElement.clientHeight, document.documentElement.clientWidth);
-
-  //  Scrolling
-  //  window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
-
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset,
-  //    top: s1coords.top + window.pageYOffset,
-  //    behavior: "smooth",
-  // });
-
-  section1.scrollIntoView({behavior: "smooth"})
-});
-
-/*
 const h1 = document.querySelector("h1");
 
 const alertH1 = function(e) {
@@ -184,5 +270,35 @@ document
 document.querySelector(".nav").addEventListener("click", function(e) {
   this.style.backgroundColor = randomColor();
   console.log("NAV", e.target, e.currentTarget);
+});
+
+
+const h1 = document.querySelector("h1");
+
+//Going downwards: child
+console.log(h1.querySelectorAll(".highlight"));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "black";
+
+//Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(".header").style.background = "var(--gradient-secondary)";
+
+h1.closest("h1").style.background = "var(--gradient-primary)";
+
+//Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function(el) {
+  if(el !== h1) el.style.tansform = 'scale(0.5)';
 });
 */
