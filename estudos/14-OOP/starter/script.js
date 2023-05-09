850670320203317be0bb1fdde2361f25c4627e79
pedroh-leite <pedrohleite.alves@gmail.com>
2023-05-09 13:51:00 -0300

    Learning OOP

--- estudos/14-OOP/starter/script.js
+++ estudos/14-OOP/starter/script.js
@@ -1,1 +1,99 @@
 'use strict';
+/*
+const Person = function(firstName, birthYear) {
+    //Instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    //Never to this
+    //  this.calcAge 
+}
+
+const jonas = new Person("Jonas", 1991);
+console.log(jonas);
+
+//1. New {} is created
+//2. Function is called, this = {}
+//3. {} linked to prototype
+//4. Function automatically return {}
+
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
+
+//Prototypes
+console.log(Person.prototype)
+
+Person.prototype.calcAge = function() {
+    console.log(2037 - this.birthYear)
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species = "Homo-sapiens";
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty("firstName"));
+console.log(jonas.hasOwnProperty("species"));
+
+console.log(jonas.__proto__);
+//Object.prototype (Top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 6, 9, 3];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function(){
+   return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector("h1");
+console.dir(x => x + 1);
+*/
+
+/////////////////////////////////////////////////
+//Chalenge 1
+
+const Car = function(make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+Car.prototype.accelerate = function() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+}
+
+Car.prototype.brake = function() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+}
+
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("Mercedes", 95);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
