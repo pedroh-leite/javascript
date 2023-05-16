2e270b3b2569132f91985ee32e85a06729c6c475
pedroh-leite <pedrohleite.alves@gmail.com>
2023-05-16 13:45:14 -0300

    Learning OOP

--- estudos/14-OOP/starter/script.js
+++ estudos/14-OOP/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const Person = function(firstName, birthYear) {
     //Instance properties
     this.firstName = firstName;
@@ -31,7 +32,7 @@ Person.hey = function() {
 Person.hey();
 
 
-/*
+
 //Prototypes
 console.log(Person.prototype)
 
@@ -106,7 +107,7 @@ bmw.accelerate();
 bmw.accelerate();
 bmw.brake();
 bmw.accelerate();
-*/
+
 
 //Class expression
 // const PersonCl = class {}
@@ -171,7 +172,7 @@ PersonCl.hey();
 
 ///////////////////////////////////////////
 // Setters and getters
-/*
+
 const account = {
     owner: "Jonas",
     movements: [200, 530, 120, 300],
@@ -252,7 +253,7 @@ console.log(ford.speedUS);
  ford.brake();
  ford.speedUS = 50;
  console.log(ford);
- */
+
 
  const Person = function(firstName, birthYear) {
     this.firstName = firstName;
@@ -268,6 +269,10 @@ console.log(ford.speedUS);
     this.course = course;
  };
 
+//  Linking prototypes
+Student.prototype = Object.create(Person.prototype)
+
+
  Student.prototype.introduce = function() {
     console.log(`My name is ${this.firstName} and i study ${this.course}`);
  };
@@ -275,3 +280,195 @@ console.log(ford.speedUS);
  const mike = new Student ("Mike", 2020, "Computer science");
  console.log(mike);
  mike.introduce();
+ mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+console.log(mike instanceof Object);
+
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
+
+
+////////////////////////////////////////////
+
+const Car = function(make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+Car.prototype.accelerate = function() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const EV = function(make, speed, charge) {
+    Car.call(this, make, speed);
+    this.charge = charge;
+}
+
+//Link prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function(chargeTo) {
+    this.charge = chargeTo;
+}
+
+EV.prototype.accelerate = function() {
+    this.speed += 20;
+    this.charge--;
+    console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`);
+};
+
+const tesla = new EV("Tesla", 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.brake();
+tesla.accelerate();
+
+
+
+class PersonCl {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+
+    // Instance method
+        // Methods will be added to .prototype property
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    }
+
+    greet(){
+        console.log(`Hey ${this.firstName}`);
+    }
+
+    get age() {
+        return 2037 - this.birthYear
+    }
+
+        // Set a property that already exists
+    set fullName(name) {
+        if(name.includes(" ")) this._fullName = name;
+        else alert(`${name} is not a full name!`)
+    }
+
+    get fullName() {
+        return this._fullName;
+    }
+
+        // Static method
+    static hey(){
+        console.log("Hey there!!");
+    }
+}
+
+class StudentCl extends PersonCl {
+    constructor(fullName, birthYear, course) {
+        // Always needs to happen first!
+        super(fullName, birthYear);
+        this.course = course;
+    }
+
+    introduce() {
+        console.log(`My name is ${this.fullName} and i study ${this.course}`);
+    }
+
+
+    calcAge() {
+        console.log(`I'm ${2037 - this.birthYear} years old, but as a student i feel more like ${2037 - this.birthYear + 10}`)
+    }
+}
+
+
+
+const martha = new StudentCl("Martha Jones", 2012, "Computer science");
+martha.introduce();
+martha.calcAge();
+  
+
+const PersonProto = {
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    },
+
+    init(fisrtName, birthYear){
+        this.firstName = fisrtName;
+        this.birthYear = birthYear;
+    },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function(firstName, birthYear, course) {
+    PersonProto.init.call(this, firstName, birthYear)
+    this.course = course;
+}
+
+StudentProto.introduce = function() {
+    console.log(`My name is ${this.fullName} and i study ${this.course}`);
+}
+
+const jay = Object.create(StudentProto);
+jay.init("Jay", 2010, "Computer science");
+jay.introduce();
+jay.calcAge();
+*/
+
+class Account {
+    constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.currency = currency;
+        this.pin = pin;
+        this.movements = [];
+        this.locale = navigator.language;
+
+        console.log(`Thanks for opening a account, ${owner}`);
+    }
+
+    // Public interface 
+    deposit(val) {
+        this.movements.push(val)
+    }
+
+    withdraw(val) {
+        this.deposit(-val)
+    }
+
+    approveLoan() {
+        return true;
+    }
+
+    requestLoan(val) {
+        if(this.approveLoan(val)) {
+            this.deposit(val);
+            console.log("Loan approved");
+        }
+    }
+
+}
+
+const acc1 = new Account("Jonas", "EUR", 1111);
+console.log(acc1);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1)
+console.log(acc1.pin);
+
