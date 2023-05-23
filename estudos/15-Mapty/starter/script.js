d20bae5c7e97c3440711284efce2c5dcd515ef68
pedroh-leite <pedrohleite.alves@gmail.com>
2023-05-23 13:53:26 -0300

    finish the project

--- estudos/15-Mapty/starter/script.js
+++ estudos/15-Mapty/starter/script.js
@@ -62,9 +62,8 @@ class Cycling extends Workout {
     }
 }
 
-const run1 = new Running([39, -12], 5.2, 24, 178);
-const cycling1 = new Cycling([39, -12], 27, 95, 523);
-console.log(run1, cycling1);
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
 
 
 ////////////////////////////////////////////////////////
@@ -84,10 +83,16 @@ class App {
     #workouts = [];
 
     constructor() {
+        // Get user's position 
         this._getPosition();
+
+        // Get data from  local storage
+        this._getLocalStorage();
+
+        // Attach event handler
         form.addEventListener("submit", this._newWorkout.bind(this));
         inputType.addEventListener("change", this._toggleElevationField);
-        containerWorkouts.addEventListener("click", this._moveToPopup.bind(this))
+        containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
     }
 
     _getPosition() {
@@ -107,7 +112,6 @@ class App {
 
             const coords = [latitude, longitude];
 
-            console.log(this);
              this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
             // console.log(map);
 
@@ -118,6 +122,10 @@ class App {
 
             // Handling clicks on map
             this.#map.on("click", this._showForm.bind(this));
+
+             this.#workouts.forEach(work => {
+                this._renderWorkoutMarker(work);
+        })
     }
 
     _showForm(mapE) {
@@ -186,7 +194,6 @@ class App {
 
             // Add new object to workout array
             this.#workouts.push(workout); 
-            console.log(workout)
 
             // Render workout on map as a marker
             this._renderWorkoutMarker(workout)
@@ -195,7 +202,10 @@ class App {
             this._renderWorkout(workout)
 
             // Hide form + xClear input fields
-            this._hideForm();          
+            this._hideForm(); 
+
+            //Set local storage to all  workouts
+            this._setLocalStorage();  
     }
 
     _renderWorkoutMarker(workout) {
@@ -267,12 +277,10 @@ class App {
 
     _moveToPopup(e) {
         const workoutEl = e.target.closest(".workout");
-        console.log(workoutEl)
 
         if(!workoutEl) return;
 
         const workout = this.#workouts.find(work => work.id === workoutEl.dataset.id);
-        console.log(workout)
 
         this.#map.setView(workout.coords, this.#mapZoomLevel, {
             animate: true,
@@ -282,7 +290,28 @@ class App {
         });
 
         // Using the public interface
-        workout.click();
+        // workout.click();
+    }
+
+    _setLocalStorage() {
+        localStorage.setItem("workouts", JSON.stringify(this.#workouts));
+    }
+
+    _getLocalStorage() {
+        const data = JSON.parse(localStorage.getItem("workouts"));
+
+        if(!data) return;
+
+        this.#workouts = data;
+
+        this.#workouts.forEach(work => {
+            this._renderWorkout(work);
+        })
+    }
+
+    reset() {
+        localStorage.removeItem("workouts");
+        location.reload();
     }
 }
 
